b28fec708e085787fe8a7554e137cae4c25c0f30
FrensysThomas87 <delta873@gmail.com>
2020-12-11 17:47:38 +0100

    corretto colore header

---
+++ java-script/script.js
@@ -0,0 +1,13 @@
+// var side_bar = document.getElementById('sidebar').classList.add('js-color-red');
+//
+// var header = document.getElementsByTagName('header');
+//
+//
+//
+// document.getElementsByClassName('install-app').classList.remove('margin-left-15px');
+//
+// side_bar.style.background = '#97DE29';
+//
+// side_bar.style.color = 'black';
+//
+// header.style.background = '#ccc';
